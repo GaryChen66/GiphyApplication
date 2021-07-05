6648a606647fab3291a47132523d2023a38bda4c
garychen66 <rising.hunter@yandex.com>
2021-07-06 00:51:31 +0800

    show favorite gifs on favorite fragment

--- app/src/main/java/com/freshworks/giphy/fragments/FavoriteFragment.kt
+++ app/src/main/java/com/freshworks/giphy/fragments/FavoriteFragment.kt
@@ -1,5 +1,7 @@
 package com.freshworks.giphy.fragments
 
+import android.os.Bundle
+import android.view.View
 import com.freshworks.giphy.repository.model.GiphyModel
 import com.freshworks.giphy.viewmodels.FavoriteViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -11,7 +13,16 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class FavoriteFragment: BaseFragment() {
     override val viewModel by viewModel<FavoriteViewModel>()
 
-    override fun setFavoriteStatus(item: GiphyModel, isFavorite: Boolean) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        viewModel.favoriteGifs.observe(viewLifecycleOwner) {
+            listAdapter.submitList(it.map(::GiphyModel))
+        }
+    }
+
+    override fun setFavoriteStatus(item:GiphyModel, isFavorite: Boolean) {
+        //Always remove from favorite
+        viewModel.removeFavoriteGif(item.id)
     }
 }
