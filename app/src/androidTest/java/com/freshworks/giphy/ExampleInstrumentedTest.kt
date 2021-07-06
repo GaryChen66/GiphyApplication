209ea3973e15eee8b04b50706ef8352bf1a5e897
garychen66 <rising.hunter@yandex.com>
2021-07-06 08:47:10 +0800

    fix unit test useAppContext

--- app/src/androidTest/java/com/freshworks/giphy/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/freshworks/giphy/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.freshworks.giphyapplication", appContext.packageName)
+        assertEquals("com.freshworks.giphy", appContext.packageName)
     }
 }
